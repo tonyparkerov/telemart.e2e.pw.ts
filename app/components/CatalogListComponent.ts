34aeb8174980d0d36ed26205ef59b7adb7782a6b
tonyparkerov <antonproskurov@gmail.com>
2025-05-03 16:14:27 +0300

    fix

--- app/components/CatalogListComponent.ts
+++ app/components/CatalogListComponent.ts
@@ -1,9 +1,11 @@
 import { BaseComponent, ProductItemComponent } from "@components";
+import { expect } from "@playwright/test";
 
 export class CatalogListComponent extends BaseComponent {
   private itemLocator = this.page.locator(".product-item");
 
   async getAllItems() {
+    await expect(this.itemLocator).not.toHaveCount(0);
     const allItemsLocators = await this.itemLocator.all();
     const result = [];
     for (const locator of allItemsLocators) {
