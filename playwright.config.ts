f0c962b1a5760346aa66de8dbc1a902d4d47d6e6
tonyparkerov <antonproskurov@gmail.com>
2025-05-03 16:00:28 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? "100%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["playwright-ctrf-json-reporter", {}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
